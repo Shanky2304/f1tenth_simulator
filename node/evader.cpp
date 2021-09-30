8e63c61668f259ca5731cbfbff7077fef3bd34da
Vivek Shankar <shankar9@buffalo.edu>
2021-09-30 14:24:53 -0400

    Altered evader logic

--- node/evader.cpp
+++ node/evader.cpp
@@ -24,7 +24,7 @@ private:
 
     double max_speed, max_steering_angle;
 
-    float safe_distance_threshold = 0.3;
+    double safe_distance_threshold = 0.3;
 
     // Listen for odom & laser scan messages
     ros::Subscriber scan_sub;
@@ -63,11 +63,11 @@ public:
 
     }
 
-    void publish_random_drive() {
+    void publish_random_drive(int max_range_index, double ang_incr) {
         ackermann_msgs::AckermannDriveStamped drive_st_msg;
         ackermann_msgs::AckermannDrive drive_msg;
 
-        drive_msg = get_random_drive();
+        drive_msg = get_random_drive(max_range_index, ang_incr);
 
         // set drive message in drive stamped message
         drive_st_msg.drive = drive_msg;
@@ -77,11 +77,17 @@ public:
     }
 
     void scan_callback(const sensor_msgs::LaserScan & lc_msg) {
-        // Here when something is very close we need to stop (set speed to zero) and then invoke odom_callback somehow.
         ackermann_msgs::AckermannDriveStamped drive_st_msg;
         ackermann_msgs::AckermannDrive drive_msg;
+        int num_of_beams = lc_msg.ranges.size();
+        int max_range_index = *std::max_element(lc_msg.ranges.begin(), lc_msg.ranges.end());
+        double ang_incr = lc_msg.angle_increment;
+//        int k = 0;
+//        double blocked_paths_incr[num_of_beams];
+//
 
         if (first_run) {
+            ros::Duration(5).sleep();
             first_run = false;
             drive_msg.speed = 2.0;
             drive_msg.steering_angle = 0.0;
@@ -93,16 +99,33 @@ public:
             drive_pub.publish(drive_st_msg);
         }
 
-        int num_of_beams = lc_msg.ranges.size();
+        // Below loop records the burst of angle increments that are not safe to proceed in.
+        // E.g. - blocked_paths_incr[0] = 4 & blocked_paths_incr[1] = 10,
+        // min + (4 * ang_incr) to min + (4 * ang_incr) are blocked angles for the car.
+//        for (int i = 0; i < num_of_beams; i++) {
+//            if (lc_msg.ranges[i] < safe_distance_threshold) {
+//                // blocked incr. begin
+//                blocked_paths_incr[k++] = i;
+//                for (int j = i; j < num_of_beams; j++) {
+//                    if (lc_msg.ranges[j] > safe_distance_threshold && j == num_of_beams)
+//                        blocked_paths_incr[k] = j;
+//                    else if (lc_msg.ranges[j] > safe_distance_threshold) {
+//                        // blocked incr. end
+//                        blocked_paths_incr[k++] = j - 1;
+//                        break;
+//                    }
+//                }
+//            }
+//        }
 
         // For each beam reading make a decision
         for (int i = 0; i < num_of_beams; i++) {
             if (lc_msg.ranges[i] < safe_distance_threshold) {
                 // collision about to happen set drive to 0.
                 // ROS_INFO_STREAM("We need to stop" << lc_msg.ranges[i]);
-                // stop_car();
+                stop_car();
                 // call random walker logic to get new Ackermann drive
-                publish_random_drive();
+                publish_random_drive(max_range_index, ang_incr);
                 break;
             }
         }
@@ -114,7 +137,7 @@ public:
         ackermann_msgs::AckermannDrive drive_msg;
 
         drive_msg.speed = 0.0;
-        drive_msg.acceleration = -10.0;
+        drive_msg.acceleration = 0.0;
         drive_msg.steering_angle = 0.0;
         drive_msg.steering_angle_velocity = 0.0;
 
@@ -123,31 +146,38 @@ public:
 
         // publish AckermannDriveStamped message to drive topic
         drive_pub.publish(drive_st_msg);
+        prev_angle = 0.0;
     }
 
-    ackermann_msgs::AckermannDrive get_random_drive() {
+    ackermann_msgs::AckermannDrive get_random_drive(int max_range_index, double ang_incr) {
         ackermann_msgs::AckermannDrive drive_msg;
         drive_msg.speed = 2.0;
 
-        /// STEERING ANGLE CALCULATION
-        // random number between 0 and 1
-        double random = ((double) rand() / RAND_MAX);
-        // good range to cause lots of turning
-        double range = max_steering_angle / 2.0;
-        // compute random amount to change desired angle by (between -range and range)
-        double rand_ang = range * random - range / 2.0;
+//        /// STEERING ANGLE CALCULATION
+//        // random number between 0 and 1
+//        double random = ((double) rand() / RAND_MAX);
+//        // good range to cause lots of turning
+//        double range = max_steering_angle / 2.0;
+//        // compute random amount to change desired angle by (between -range and range)
+//        double rand_ang = range * random - range / 2.0;
+//
+//        // sometimes change sign so it turns more (basically add bias to continue turning in current direction)
+//        random = ((double) rand() / RAND_MAX);
+//        if ((random > .8) && (prev_angle != 0)) {
+//            double sign_rand = rand_ang / std::abs(rand_ang);
+//            double sign_prev = prev_angle / std::abs(prev_angle);
+//            rand_ang *= sign_rand * sign_prev;
+//        }
+//
+//        // set angle (add random change to previous angle)
+//        int blocked_range_size = blocked_ang_incr.size();
+//        for (int i = 0; i < blocked_range_size; i++) {
+//
+//        }
 
-        // sometimes change sign so it turns more (basically add bias to continue turning in current direction)
-        random = ((double) rand() / RAND_MAX);
-        if ((random > .8) && (prev_angle != 0)) {
-            double sign_rand = rand_ang / std::abs(rand_ang);
-            double sign_prev = prev_angle / std::abs(prev_angle);
-            rand_ang *= sign_rand * sign_prev;
-        }
-
-        // set angle (add random change to previous angle)
-        drive_msg.steering_angle = std::min(std::max(prev_angle + rand_ang, -max_steering_angle), max_steering_angle);
-
+        double steering_angle = std::min(std::max(-max_steering_angle + (max_range_index * ang_incr), -max_steering_angle)
+                , max_steering_angle);
+        drive_msg.steering_angle = steering_angle;
         // reset previous desired angle
         prev_angle = drive_msg.steering_angle;
 
